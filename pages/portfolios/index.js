fb2652c4af7643ab9d2b81c87af260c3affdd26d
anitkumarsingh <anikumarsingh99@gmail.com>
2021-06-05 09:08:01 +0530

    refactor

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -36,37 +36,6 @@ export const PortfolioItems = ({ data }) => {
                 </div>
               );
             })}
-
-          {/* <div className="col-md-4"> */}
-          {/* <div className="card subtle-shadow no-border"> */}
-          {/* <div className="card-body"> */}
-          {/* <h5 className="card-title">Card title</h5> */}
-          {/* <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
-          {/* <p className="card-text fs-2 "> */}
-          {/* Some quick example text to build on the card title and make up */}
-          {/* the bulk of the card's content. */}
-          {/* </p> */}
-          {/* </div> */}
-          {/* <div className="card-footer no-border"> */}
-          {/* <small className="text-muted">Last updated 3 mins ago</small> */}
-          {/* </div> */}
-          {/* </div> */}
-          {/* </div> */}
-          {/* <div className="col-md-4"> */}
-          {/* <div className="card subtle-shadow no-border"> */}
-          {/* <div className="card-body"> */}
-          {/* <h5 className="card-title">Card title</h5> */}
-          {/* <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
-          {/* <p className="card-text fs-2 "> */}
-          {/* Some quick example text to build on the card title and make up */}
-          {/* the bulk of the card's content. */}
-          {/* </p> */}
-          {/* </div> */}
-          {/* <div className="card-footer no-border"> */}
-          {/* <small className="text-muted">Last updated 3 mins ago</small> */}
-          {/* </div> */}
-          {/* </div> */}
-          {/* </div> */}
           <div className="col-md-12">
             <br />
             <br />
